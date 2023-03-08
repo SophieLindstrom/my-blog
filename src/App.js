2aae4d7ffb5b26702da4b7b3dbc028d81bcdea15
Sophie <sophie.lindstrom90@gmail.com>
2023-03-08 12:58:36 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Post from "./pages/Post";
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <Post name="Sophie" />
+    </div>
+  );
+}
+
+export default App;
